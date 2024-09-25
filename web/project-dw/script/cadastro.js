635a171c943cea987de77475f0524fa4da9d9938
Mpfg05 <marcos.garcia@aluno.faculdadeimpacta.com.br>
2024-09-24 23:01:54 -0300

    Update cadastro.js

--- web/project-dw/script/cadastro.js
+++ web/project-dw/script/cadastro.js
@@ -1,28 +1,70 @@
-const url = "https://go-wash-api.onrender.com/api/user"; 
-async function cadastroUsuario(){
-    let name = document.getElementById('name').value
-    let email = document.getElementById('email').value
-    let api = await fetch(url,{
-        method:"POST",
-        body:JSON.stringify({
-            "name":name,
-            "email":email,
-            "user_type_id":1,
-            "password": "123456",
-            "cpf_cnpj": "00188338020",
-            "terms": 1,
-            "birthday":"2000-10-12"    
-        }),
-        headers:{
-            'Content-Type':'application/json'
-        }
-    });
+const url = 'https://go-wash-api.onrender.com/api/user'
+async function cadastro(){
+    let name = document.getElementById('name').value;
+    let terms = document.getElementById('terms').checked;
+    let cpf_cnpj = document.getElementById('cpf_cnpj').value;
+    let email = document.getElementById ('email').value;
 
-    if(api.ok){
-        let resposta = await api.json();
-        console.log(resposta)
-        return
+    /* if (name){
+        alert("Olá " + name)
+    }else{
+        alert("Nome é obrigatório")
     }
-    let respostaErro = await api.json();
-        console.log(respostaErro.data.errors.cpf_cnpj)
+
+    if (terms){
+        alert(terms)
+    }else{
+        alert("Precisa marcar a caixa Termos ")
+    }
+    
+}*/
+    let api = await fetch(url,{
+        method: "POST", 
+        body:JSON.stringify(
+            {
+                "name":name,
+                "email":email,
+                "user_type_id":1,
+                "password": "123456",
+                "cpf_cnpj": cpf_cnpj,
+                "terms": 1,
+                "birthday":"2000-10-12"    
+            }
+         ),
+         headers:{
+            'Content-Type': 'application/json'      
+         }
+    })
+
+    if (api.ok){
+        let resposta = await api.json();
+        console.log (resposta)
+
+        return;
+    }
+
+    console.log("Erro");
+
+
 }
+
+
+
+    if (api.ok){
+        let resposta = await api.json();
+        console.log (resposta)
+        return;
+    }
+    let respostErrors = await api.json();
+    alert (respostErrors.data.errors.cpf_cnpj[0])
+    0   
+    console.log("erro");
+    alert(terms);
+
+
+    if(name){
+        alert(nome);
+        }else{
+            alert("O nome é obrigatório");
+        }
+
