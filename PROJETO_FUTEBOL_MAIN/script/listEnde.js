528ea38587a8d1542d45f1d8401c95d0334f9557
Mpfg05 <marcos.garcia@aluno.faculdadeimpacta.com.br>
2024-10-31 08:55:04 -0300

    Update listEnde.js

--- PROJETO_FUTEBOL_MAIN/script/listEnde.js
+++ PROJETO_FUTEBOL_MAIN/script/listEnde.js
@@ -3,23 +3,53 @@ const url = 'https://go-wash-api.onrender.com/api/auth/address';
 async function listarEndereco() {
     let userToken = JSON.parse(localStorage.getItem("user")).access_token;
 
-    let api = await fetch(url, {
-        method: "GET",
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + userToken
-        }
-    });
+    try {
+        let api = await fetch(url, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + userToken
+            }
+        });
 
-    if (api.ok) {
-        let resposta = await api.json();
-        console.log(resposta.data)
-        return resposta
-    } else {
-        let respostaErro = await api.json();
-        console.error(respostaErro);
-        alert('Erro ao buscar os endereços.');
+        if (api.ok) {
+            let resposta = await api.json();
+            let enderecos = resposta.data; 
+            
+            
+            preencherTabelaEnderecos(enderecos);
+        } else {
+            let respostaErro = await api.json();
+            console.error(respostaErro);
+            alert('Nenhum endereço cadastrado.');
+        }
+    } catch (error) {
+        console.log("Erro na requisição:", error);
     }
 }
 
-listarEndereco()
+
+function preencherTabelaEnderecos(enderecos) {
+    const enderecosBody = document.getElementById('enderecosBody');
+    enderecosBody.innerHTML = ''; 
+
+    
+    enderecos.forEach(endereco => {
+        let row = document.createElement('tr'); 
+
+        
+        row.innerHTML = `
+            <td>${endereco.title}</td>
+            <td>${endereco.address}</td>
+            <td>${endereco.number}</td>
+            <td>${endereco.cep}</td>
+            <td>${endereco.complement}</td>
+        `;
+
+        
+        enderecosBody.appendChild(row);
+    });
+}
+
+
+document.querySelector('button').addEventListener('click', listarEndereco);
